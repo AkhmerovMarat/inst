d93fa590c93c00733a84416444b2b526cbb8f529
Student <student@itpark>
2019-09-28 10:57:56 +0300

    Переименование

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log('worked');
 //абстракция -  убираем все лишнее что не нужно для решения данной задачи
 
-const author = {
+const buzova = { // F2
 id: 'buzova86',
 avatarUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com',
 verified: true,
