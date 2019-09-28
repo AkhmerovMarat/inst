66d64c516292ab1ff6177da908988cc2d547951a
Student <student@itpark>
2019-09-28 10:42:38 +0300

    photos and video

---
+++ app.js
@@ -0,0 +1,30 @@
+console.log('worked');
+//абстракция -  убираем все лишнее что не нужно для решения данной задачи
+
+const author = {
+id: 'buzova86',
+avatarUrl: 'https://scontent-arn2-1.cdninstagram.com/vp/e24c6f401983834e27247c3542022660/5E1B7CB0/t51.2885-19/s150x150/69672885_368328154091538_8485268536803983360_n.jpg?_nc_ht=scontent-arn2-1.cdninstagram.com',
+verified: true,
+}
+
+const photo = {
+     id : 'B23d82NooNF',
+     location: 'Набережная',
+     imageUrl: 'https://placekitten.com/800/800',
+     videoUrl: null,
+     likes : 159594,
+
+}
+
+const video = {
+id: 'B24Flbjos5E',
+author: author,
+location: '',
+imageUrl: 'https//placekitten.com/500/500',
+videoUrl: null,
+likes: 15964,
+
+}
+
+
+
